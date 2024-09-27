0bbf4f0a1603b7a1f479de00ea1b0254b6b5b475
Thanassis Avgerinos <thanassis.avgerinos@gmail.com>
2024-09-27 15:54:36 -0400

    More fixes.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ BRIDGE_INTERFACE=${BRIDGE_INTERFACE:-ens5}
 iptables -I INPUT -p tcp --dport 80 -j NFQUEUE --queue-num 0
 iptables -I OUTPUT -p tcp --sport 80 -j NFQUEUE --queue-num 0
 
-echo $BRIDGE_INTERFACE
+#echo $BRIDGE_INTERFACE
 # Start Snort on the detected interface
 stdbuf -i0 -o0 -e0 snort -Q -A console -q -c /etc/snort/snort.conf --daq nfq --daq-var queue=0 --daq-mode inline
 
