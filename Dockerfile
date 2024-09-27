c1f1a6e0bb1150627a25c943a0b910897e1b09e4
Thanassis Avgerinos <thanassis.avgerinos@gmail.com>
2024-09-27 14:32:38 -0400

    Working.

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,15 @@ FROM ubuntu:24.04
 
 RUN apt-get update && apt-get install -fy snort iproute2
 
+RUN apt install -fy snort-rules-default iptables libnetfilter-queue1
+
 # Expose the port for monitoring
 EXPOSE 80
 
 COPY docker-entrypoint.sh /
+
 COPY local.rules /etc/snort/rules/
+COPY snort.conf /etc/snort/
 
 # Run Snort
 CMD ["/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,9 +5,12 @@
 set -euxo pipefail
 
 #BRIDGE_INTERFACE=$(ip link | grep -o 'eth0-[^ :]*')
-BRIDGE_INTERFACE=${BRIDGE_INTERFACE:-eth0}
+BRIDGE_INTERFACE=${BRIDGE_INTERFACE:-ens5}
+
+iptables -I INPUT -p tcp --dport 80 -j NFQUEUE --queue-num 0
+iptables -I OUTPUT -p tcp --sport 80 -j NFQUEUE --queue-num 0
 
 echo $BRIDGE_INTERFACE
 # Start Snort on the detected interface
-snort -A console -q -c /etc/snort/snort.conf -i $BRIDGE_INTERFACE
+stdbuf -i0 -o0 -e0 snort -Q -A console -q -c /etc/snort/snort.conf --daq nfq --daq-var queue=0 --daq-mode inline
 
